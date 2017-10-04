d7803df5a265744347bdf1b42b9f98e5d3a906a6
Feafortecsy <31724313+Feafortecsy@users.noreply.github.com>
2017-10-04 13:19:36 +0800

    2017-10-04
    Bootstrsp-Scroll image advertising

--- Bootstrsp-Scroll image advertising/js/Scroll image advertising.js
+++ Bootstrsp-Scroll image advertising/js/Scroll image advertising.js
@@ -1,7 +1,16 @@
 
 
-  $(function () {
-  	   $('.carousel').carousel({
-    	  interval: 2000
-     });
-  });
+ $(function () {
+        $('#ad-carousel').carousel();
+        $('#menu-nav .navbar-collapse a').click(function (e) {
+            var href = $(this).attr('href');
+            var tabId = $(this).attr('data-tab');
+            if ('#' !== href) {
+                e.preventDefault();
+                $(document).scrollTop($(href).offset().top - 70);
+                if (tabId) {
+                    $('#feature-tab a[href=#' + tabId + ']').tab('show');
+                }
+            }
+        });
+    });
